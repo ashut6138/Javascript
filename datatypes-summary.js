33cd19726d02933ef9064ce604170e6ceba4aeb4
ashut6138 <ashut6138@gmail.com>
2025-02-26 15:51:28 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,67 @@
+// "Primitive"
+
+// .......7 Types: 
+// string, --- 
+// Number, 
+// Boolean, 
+// Null(empty), 
+// Undefined, 
+// Symbol(Anything to make the value unique), 
+// BigInt 
+
+//...........delaration:
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // OR let userEmail = undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId); // false
+
+const bigNumber = 673686846286876n
+
+//.........RETURN TYPES:
+
+console.log(typeof score) // number
+console.log(typeof scoreValue) // number
+console.log(typeof isLoggedIn) // boolean
+console.log(typeof outsideTemp) // object
+console.log(typeof userEmail) // undefined
+console.log(typeof id) // symbol
+console.log(typeof anotherId) // symbol
+ console.log(typeof bigNumber) // bigInt
+
+// "Reference" (Non-Primitive)
+
+//....... 3 Types: (All returns types are OBJECT)
+// Array
+//Objects
+// Functions.--- (object function)
+// master of javascript --> object, browser event.
+
+// ................Declaration:
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+//............Return types:
+
+console.log(typeof myFunction); // function
+console.log(typeof heros); // object
+console.log(typeof myObj) // object
+
+
+//STUDY
+//  "https://262.ecma-international.org/5.1/#sec-11.4.3"
