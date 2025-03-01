24090400b15a70fa931b43266603612a75530f4a
ashut6138 <ashut6138@gmail.com>
2025-03-01 17:15:49 +0000

    datesin JS

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,15 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString);
+console.log(myDate.toLocaleString());
+console.log(typeof myDate); // object
+
+// let myCreateDate = new Date(2025, 3, 16)
+// console.log(myCreateDate.toDateString())
+
+
+let myCreateDate = new Date(2025, 3, 16, 5, 3)
+console.log(myCreateDate.toLocaleString());
+
