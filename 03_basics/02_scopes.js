3641fb3c0615c5b8a360f9bb096a160159e38dac
ashut6138 <ashut6138@gmail.com>
2025-03-13 11:34:13 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,152 @@
+/*
+let a = 10
+const b = 20
+var c = 30
+
+console.log(a)
+console.log(b)
+console.log(c)
+*/
+
+/*
+var c = 3000
+if (true)
+{
+    let a = 10
+    const b = 20
+    c = 30
+}
+
+// console.log(a)
+// console.log(b)
+console.log(c)      // 30
+*/
+
+
+/*
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+}
+console.log(a);     // 300
+*/
+
+
+/*
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);      // 10
+}
+console.log(a);     300  
+*/
+
+
+//---------------"NESTED SCOPE"
+
+
+/*
+function one() {
+    const username = "hitesh"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);      // hitesh
+    }
+    // console.log(website); // error
+
+    two()
+}
+
+// one()
+*/
+
+
+/*
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube "
+        console.log(username + website);        // hitesh youtube
+    }
+    // console.log(website);        // error
+}
+// console.log(username);      // error
+*/
+
+
+// +++++++++++++++++++++++++++++++ INTERESTING ++++++++++++++++++++++
+
+function addone
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
